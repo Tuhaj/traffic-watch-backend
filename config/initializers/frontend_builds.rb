2f2df88319576e98b6628f0be5f0d616bc7b5461
piotrzientara <piotr.zientara@rebased.pl>
2015-02-27 07:21:45 +0100

    authentication required to change frontend

--- config/initializers/frontend_builds.rb
+++ config/initializers/frontend_builds.rb
@@ -1,7 +1,7 @@
-require FrontEndBuilds::Engine.root.join('app/controllers/front_end_builds/application_controller.rb')
+require FrontEndBuilds::Engine.root.join('app/controllers/front_end_builds/admin_controller.rb')
 
-class FrontEndBuilds::ApplicationController
+class FrontEndBuilds::AdminController
 
-  # http_basic_authenticate_with name: ENV['admin_login'], password: ENV['admin_password']
+  http_basic_authenticate_with name: ENV['admin_login'], password: ENV['admin_password']
 
 end
