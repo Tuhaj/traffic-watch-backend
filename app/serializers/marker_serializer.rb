b5b3c7bca10509f10e405da695cb537c7776c6c9
piotrzientara <piotr.zientara@rebased.pl>
2015-03-08 14:44:59 +0100

    adds current load to marker serializer

--- app/serializers/marker_serializer.rb
+++ app/serializers/marker_serializer.rb
@@ -1,17 +1,21 @@
 class MarkerSerializer < ActiveModel::Serializer
   self.root = true
 
-  attributes :id, :lat, :lng, :time_without_traffic
+  attributes :id, :lat, :lng
 
   # has_many :samples, embed: :objects # used when the ralation was not async
 
+  attributes :current_load
   # Attributes computed by serializer
   attributes :links
 
   def links
     {
       samples: url_helpers.marker_samples_path(object),
-
     }
+  end
+
+  def current_load
+    object.samples.last.traffic_load_percentage
   end
 end
