15ae3935a670a8ce89996b60ae7de113a49c8b69
piotrzientara <piotr.zientara@rebased.pl>
2015-03-20 18:59:28 +0100

    stats time bugfix

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,7 +2,7 @@ class StatsController < ApplicationController
   respond_to :json
 
   def show
-    render json: City.find_by_name(city_params[:name]).stats.where('created_at > ?', 7.days.ago), each_serializer: StatSerializer
+    render json: City.find_by_name(city_params[:name]).stats.where('created_at > ?', 6.days.ago.midnight), each_serializer: StatSerializer
   end
 
   private
