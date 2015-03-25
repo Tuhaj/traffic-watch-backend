915d3e99457056b312f217a14eb1d06aadf7ac5c
piotrzientara <piotr.zientara@rebased.pl>
2015-03-25 20:47:20 +0100

    unicorn production.rb improvement

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,25 +1,30 @@
-# Minimal sample configuration file for Unicorn (not Rack) when used
-# with daemonization (unicorn -D) started in your working directory.
-#
-# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
-# documentation.
-# See also http://unicorn.bogomips.org/examples/unicorn.conf.rb for
-# a more verbose configuration using more features.
 
-# listen 3000 # by default Unicorn listens on port 8080 I comment it out so gets request from NginX
-
-worker_processes 2 # this should be >= nr_cpus
+worker_processes 2
 pid "/home/piotr/traffic-watch-backend/shared/tmp/pids/unicorn.pid"
 stderr_path "/home/piotr/traffic-watch-backend/shared/log/unicorn.log"
 stdout_path "/home/piotr/traffic-watch-backend/shared/log/unicorn.log"
 
+working_directory '/home/piotr/traffic-watch-backend/current'
+
+listen "/home/piotr/traffic-watch-backend/shared/tmp/sockets/unicorn.sock", :backlog => 64
+
 preload_app       true
 
-# Help ensure your application will always spawn in the symlinked
-# "current" directory that Capistrano sets up.
-working_directory '/home/piotr/traffic-watch-backend/current'
-#not tested yet
-listen "/home/piotr/traffic-watch-backend/shared/tmp/sockets/unicorn.sock", :backlog => 64
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  #defined?(ActiveRecord::Base) and
+  #  ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
 
 #unicorn workers pids in ~/app/shared/tmp/pids/ directory
 after_fork do |server, worker|
