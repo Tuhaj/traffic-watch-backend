68d8409fac17abe0f251a5d16055670018527504
piotrzientara <piotr.zientara@rebased.pl>
2015-03-26 21:40:43 +0100

    clear markers controller

--- app/controllers/markers_controller.rb
+++ app/controllers/markers_controller.rb
@@ -1,28 +1,22 @@
 class MarkersController < ApplicationController
   respond_to :json
 
-  # use if async samples needed
-
-  # def samples
-  #   render json: { Marker.find(marker_params[:marker_id]).samples.where('created_at > ?', 6.days.ago.midnight).select{|sample| sample.created_at.min < 5} }
-  # end
-
-  def load
-    date = Time.parse(load_params[:date])
+  def sample
+    marker = Marker.find(marker_params[:id])
+    date = Time.parse(marker_params[:date])
     margin = 8.minutes
-    marker = Marker.find(load_params[:marker_id])
-    sample = marker.samples.where('created_at >= ? and created_at < ?', date - margin, date + margin).last
+    sample = marker.samples.where(created_at: (date - margin)..(date + margin)).last
+
     render json: { load: sample.traffic_load_percentage }
+
+  rescue ActiveRecord::RecordNotFound
+    render json: { message: 'Sample not found' }, status: 404
   end
 
   private
 
   def marker_params
-    params.permit(:marker_id)
-  end
-
-  def load_params
-    params.permit(:marker_id, :date)
+    params.permit(:id, :date)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,12 @@ Rails.application.routes.draw do
   get '/city/:name'              => 'cities#show',     as: 'city'
   get '/city/stats/:id'          => 'cities#stats',    as: 'city_stats'
   get '/stats/:name'             => 'stats#show',      as: 'stat'
-  get '/samples/:marker_id'      => 'markers#samples', as: 'marker_samples'
-  get '/marker_load/:marker_id'  => 'markers#load',    as: 'marker_load'
+
+  resources :markers, only: [] do
+    member do
+      get :sample
+    end
+  end
 
   front_end 'traffic-watch', '/'
 end
