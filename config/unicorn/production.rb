e1309c09cd67ac942457c831f233ec3895eb60f3
piotrzientara <piotr.zientara@rebased.pl>
2015-03-31 00:22:26 +0200

    clean up of unicorn file

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,20 +1,7 @@
 
 worker_processes 2
-pid "/home/piotr/traffic-watch-backend/shared/tmp/pids/unicorn.pid"
-stderr_path "/home/piotr/traffic-watch-backend/shared/log/unicorn.log"
-stdout_path "/home/piotr/traffic-watch-backend/shared/log/unicorn.log"
-
-working_directory '/home/piotr/traffic-watch-backend/current'
-
-listen "/home/piotr/traffic-watch-backend/shared/tmp/sockets/unicorn.sock", :backlog => 64
-
 preload_app       true
-
 before_fork do |server, worker|
-  # the following is highly recomended for Rails + "preload_app true"
-  # as there's no need for the master process to hold a connection
-  #defined?(ActiveRecord::Base) and
-  #  ActiveRecord::Base.connection.disconnect!
 
   old_pid = "#{server.config[:pid]}.oldbin"
   if old_pid != server.pid
@@ -26,7 +13,6 @@ before_fork do |server, worker|
   end
 end
 
-#unicorn workers pids in ~/app/shared/tmp/pids/ directory
 after_fork do |server, worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
   child_pid = server.config[:pid].sub('.pid', ".#{worker.nr}.pid")
