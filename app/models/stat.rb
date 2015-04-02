04e0f2d72155e8ba8a514a41702f0d9b3c2ea0b4
piotrzientara <piotr.zientara@rebased.pl>
2015-04-02 23:21:06 +0200

    remove old code

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,15 +1,3 @@
 class Stat < ActiveRecord::Base
   belongs_to :city
-
-  def average_time_to_center_min
-    weighted_mean.to_i / 60
-  end
-
-  def to_raport
-    "#{ created_at }.............#{ weighted_mean }"
-  end
-
-  def to_raport_min
-    "#{ created_at }.............#{ average_time_to_center_min }"
-  end
 end
