98748ab446050ac7ff95c76954f62ebe5772787b
piotrzientara <piotr.zientara@rebased.pl>
2015-06-14 18:02:37 +0200

    refactors cities controller

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -6,39 +6,38 @@ class CitiesController < ApplicationController
   end
 
   def stats
-    if city = City.find_by_name(city_params[:name])
-      stats = city.last_week_stats
-      render json: { stats: stats, each_serializer: StatSerializer }
-    else
-      render json: { message: 'Record not found' }, status: 404
-    end
+    city = get_city
+    return render json: { message: 'Record not found' },
+      status: 404 unless city
+
+    stats = city.last_week_stats
+    render json: { stats: stats, each_serializer: StatSerializer }
   end
 
   def traffic
-    raise ArgumentError, "No time argument" unless city_params[:time]
+    return render json: { message: 'Record not found' },
+      status: 404 unless city = get_city
 
-    traffic_load = {}
-    city = City.find_by_name(city_params[:name])
+    return render json: { message: 'No time argument' },
+      status: 400 unless time = city_params[:time]
+
+    time = Time.parse(time)
     markers = city.markers
-    margin = 8.minutes
-    time = Time.parse(city_params[:time])
-    markers.each do |marker|
-      traffic_load[marker.id] = marker.samples
-        .where(created_at: (time - margin)..(time + margin)).last.traffic_load
+
+    traffic_load = markers.map { |m| [ m[:id], m.load(time) ] }.to_h
+
+    if traffic_load.empty?
+      render json: { message: 'Data not found' }, status: 404
+    else
+      render json: { load: traffic_load }
     end
-
-  raise ActiveRecord::RecordNotFound if traffic_load.empty?
-    render json: { load: traffic_load }
-
-  rescue ActiveRecord::RecordNotFound
-    render json: { message: 'Samples not found' }, status: 404
-
-  rescue ArgumentError
-    render json: { message: 'No time given' }, status: 404
-
   end
 
   private
+
+  def get_city
+    City.find_by_name(city_params[:name])
+  end
 
   def city_params
     params.permit(:name, :time)
--- app/models/marker.rb
+++ app/models/marker.rb
@@ -3,10 +3,14 @@ class Marker < ActiveRecord::Base
   has_many :samples
   has_many :polylines
 
+  MARGIN = 8.minutes
+
   def to_location
-    [
-      lat,
-      lng
-    ].join(',')
+    [ lat, lng ].join(',')
+  end
+
+  def load(time)
+    samples.where(created_at: (time - MARGIN)..(time + MARGIN)).last
+      .traffic_load
   end
 end
